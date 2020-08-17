0cd73fcb9bb6f55d117285baea7544754d7c5b61
Nikita Lenivenko <nikitagl2001@gmail.com>
2020-08-17 20:18:10 +0300

    Business Changed

---
+++ src/EasyBudget/Common/Business/Operations/IBudgetRequestOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyBudget.Common.Model;
+using EasyBudget.Common.Model.Security;
+
+namespace EasyBudget.Common.Business.Operations
+{
+    public interface IBudgetRequestOperations
+    {
+        void AddBudgetRequest(BudgetRequest request, User user);
+        void EditBudgetRequest(BudgetRequest request);
+        void DeleteBudgetRequest(Guid id);
+
+        BudgetRequest ViewingBudgetRequest(Guid id);
+
+        List<BudgetRequest> ViewingBudgetRequestsList(User user);
+    }
+}
---
+++ src/EasyBudget/Common/Business/Services/IBudgetRequestService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyBudget.Common.Model;
+using EasyBudget.Common.Model.Security;
+
+namespace EasyBudget.Common.Business.Services
+{
+    interface IBudgetRequestService
+    {
+        void AddBudgetRequest(BudgetRequest request, User user);
+        void EditBudgetRequest(Guid id);
+        void DeleteBudgetRequest(Guid id);
+
+        BudgetRequest ViewingBudgetRequest(Guid id);
+
+        List<BudgetRequest> ViewingBudgetRequestsList(User user);
+    }
+}
