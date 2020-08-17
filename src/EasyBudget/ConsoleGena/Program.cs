61093aad8caa161d19fe337cdd44d39ef7850cd4
unknown <gena.vl@gmail.com>
2020-08-17 13:16:53 +0300

    Added Template

---
+++ src/EasyBudget/ConsoleGena/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/EasyBudget/ConsoleMykyta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMykyta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
