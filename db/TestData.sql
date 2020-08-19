86d1a5b5ec340518adc9a24b08cc6d67b88abcd0
gena <gena.vl@gmail.com>
2020-08-19 15:30:19 +0300

    Sql script

---
+++ db/TestData.sql
@@ -0,0 +1,10 @@
+USE [EasyBudget]
+GO
+
+
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (N'1ede313d-a531-4e79-ab34-06270b7bdb0c', N'OriginatorEditBudgetRequest')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (N'b03d9311-6938-4cf4-98c5-801e18d37d58', N'OriginatorDeleteBudgetRequest')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (N'0af011ae-6681-47e8-a408-8bfef004208a', N'OriginatorViewBudgetRequest')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (N'8863e973-d2d6-4c02-ad92-a4dc37706c6f', N'OriginatorAddBudgetRequest')
+
+INSERT [dbo].[Department] ([Id], [Name]) VALUES (N'22946ba4-b06c-4d9e-a0d3-2e03b62afb5c', N'My dept')
