a1512a78eb798892aa6ade162fbc5c8e032712ef
gena <gena.vl@gmail.com>
2020-09-08 16:23:55 +0300

    Added services

---
+++ src/EasyBudget/Business/Services/BudgetRequestListDirectorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyBudget.Common.Business.Services;
+using EasyBudget.Common.DataAccess.Dtos;
+
+namespace EasyBudget.Business.Services
+{
+    public class BudgetRequestListDirectorService : IBudgetRequestListService
+    {
+        public List<BudgetRequestMainListDto> GetList(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/EasyBudget/Common/Business/Services/IBudgetRequestListService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyBudget.Common.DataAccess.Dtos;
+
+namespace EasyBudget.Common.Business.Services
+{
+    public interface IBudgetRequestListService
+    {
+        List<BudgetRequestMainListDto> GetList(Guid userId);
+    }
+}
