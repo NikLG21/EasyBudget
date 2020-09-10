0a03f4220d39882cfa58e9aba605ca0140133ada
Nikita Lenivenko <nikitagl2001@gmail.com>
2020-09-10 20:09:57 +0300

    BudgetListUI

--- src/EasyBudget/Presentation/ViewModels/BudgetRequestListViewModel.cs
+++ src/EasyBudget/Presentation/ViewModels/BudgetRequestListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using EasyBudget.Common.Business;
 using EasyBudget.Common.Business.Services;
 using EasyBudget.Common.DataAccess.Dtos;
@@ -10,8 +11,21 @@ namespace EasyBudget.Presentation.ViewModels
 {
     public class BudgetRequestListViewModel: IBudgetRequestListViewModel
     {
-        private UserMainInfoDto userInfo;
-        private Role role;
+        private UserMainInfoDto userInfo = new UserMainInfoDto()
+        {
+            Id = Guid.Parse("3148ce2c-540e-4cc4-a372-42e0c29a478b"),
+            CurrentRoleId = Guid.Parse("cb294f90-3cb6-4169-9916-1e39d7f4bd3d"),
+            CurrentRoleName = "Ініціатор запиту",
+        };
+
+        private Role role = new Role()
+        {
+            Actions = null,
+            Department = null,
+            Id = Guid.Parse("cb294f90-3cb6-4169-9916-1e39d7f4bd3d"),
+            Name = "Ініціатор запиту",
+            Users = new List<User>()
+        };
 
         private IBudgetRequestListServiceFactory _budgetRequestListServiceFactory;
         public List<BudgetRequestMainListDto> BudgetRequests { get; }
