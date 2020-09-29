677a12053afa580f3c890544fdd5648892e44ce4
Nikita Lenivenko <nikitagl2001@gmail.com>
2020-09-29 18:31:37 +0300

    RequestService

---
+++ src/EasyBudget/Business/Services/BudgetRequestService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyBudget.Common.Business.Services;
+using EasyBudget.Common.DataAccess;
+using EasyBudget.Common.DataAccess.Dtos;
+using EasyBudget.Common.DataAccess.Queries;
+using EasyBudget.Common.Exceptions;
+using EasyBudget.Common.Model;
+using EasyBudget.Common.Model.Security;
+
+namespace EasyBudget.Business.Services
+{
+    public class BudgetRequestService : IBudgetRequestService
+    {
+        private readonly IBudgetRequestAccess _budgetRequestAccess;
+        private readonly IUserAccess _userAccess;
+
+        public BudgetRequestService(IBudgetRequestAccess budgetRequestAccess, IUserAccess userAccess)
+        {
+            _budgetRequestAccess = budgetRequestAccess;
+            _userAccess = userAccess;
+        }
+
+        public void AddRequest(Guid userId,BudgetRequest request)
+        {
+            try
+            {
+                if (request.Name == null)
+                {
+                    throw new LackMandatoryInformation("Назва запиту");
+                }
+                User user = _userAccess.Get(userId);
+                request.State = BudgetState.Requested;
+                request.DateRequested = DateTime.Today;
+                request.Requester = user;
+                request.Unit = user.Unit;
+                _budgetRequestAccess.Add(request);
+            }
+            catch (DuplicateEntryException)
+            {
+                throw;
+            }
+            catch (CriticalException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new CriticalException(e);
+            }
+        }
+        public void AddRequestByAdmin(Guid userId, Guid id, BudgetRequest request)
+        {
+            try
+            {
+                User user = _userAccess.Get(id);
+                request.State = BudgetState.Requested;
+                request.DateRequested = DateTime.Today;
+                request.Requester = user;
+                request.Unit = user.Unit;
+                _budgetRequestAccess.Add(request);
+            }
+            catch (DuplicateEntryException)
+            {
+                throw;
+            }
+            catch (CriticalException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new CriticalException(e);
+            }
+        }
+        public void UpdateByRequester(Guid userId,BudgetRequest request)
+        {
+            try
+            {
+                if (request.State == BudgetState.Requested)
+                {
+                    _budgetRequestAccess.Update(request);
+                }
+                else
+                {
+                    throw new NonDeletedUpdatedRequestException("Оновлення");
+                }
+                
+            }
+            catch (DuplicateEntryException)
+            {
+                throw;
+            }
+            catch (CriticalException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new CriticalException(e);
+            }
+        }
+        public void DeleteBudgetRequest(Guid userId,BudgetRequest request)
+        {
+            try
+            {
+                BudgetRequest request1 = Get(userId, request.Id);
+                if (request != request1)
+                {
+                    throw new EntityUpdatedException("Запит");
+                }
+                if (request.State == BudgetState.Requested)
+                {
+                    _budgetRequestAccess.Delete(request.Id);
+                }
+                else
+                {
+                    throw new NonDeletedUpdatedRequestException("Видалення");
+                }
+            }
+            catch (CriticalException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new CriticalException(e);
+            }
+        }
+        public BudgetRequest Get(Guid userId,Guid requestId)
+        {
+            try
+            {
+                return _budgetRequestAccess.Get(requestId);
+            }
+            catch (CriticalException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new CriticalException(e);
+            }
+        }
+    }
+}
---
+++ src/EasyBudget/Common/Business/Services/IBudgetRequestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyBudget.Common.DataAccess.Dtos;
+using EasyBudget.Common.Model;
+
+namespace EasyBudget.Common.Business.Services
+{
+    public interface IBudgetRequestService
+    {
+        void AddRequest(Guid userId, BudgetRequest request);
+        void AddRequestByAdmin(Guid userId, Guid id, BudgetRequest request);
+        void UpdateByRequester(Guid userId,BudgetRequest request);
+        void DeleteBudgetRequest(Guid userId,BudgetRequest request);
+        BudgetRequest Get(Guid userId,Guid requestId);
+    }
+}
