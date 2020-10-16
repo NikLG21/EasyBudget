e0f9390d3b483c6577ec0e4852ddda6ea816c473
gena <gena.vl@gmail.com>
2020-10-16 12:31:29 +0300

    Comments

--- src/EasyBudget/Presentation/Interfaces/IBudgetRequestViewModel.cs
+++ src/EasyBudget/Presentation/Interfaces/IBudgetRequestViewModel.cs
@@ -8,6 +8,7 @@ namespace EasyBudget.Presentation.Interfaces
     public interface IBudgetRequestViewModel
     {
         BudgetRequest BudgetRequest { get; set; }
+        //TODO: I think this is not enough. We need flags for editing different fields
         bool IsEditable { get; set; }
         bool InEditMode { get; set; }
     }
